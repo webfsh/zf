026690489361014bb7a68e4600caa9a314181970
webfsh <82840396+webfsh@users.noreply.github.com>
2022-03-18 23:23:24 +0800

    Create zf1.sh

---
+++ zf1.sh
@@ -0,0 +1,46 @@
+apt update -y
+service firewalld stop
+systemctl disable firewalld
+setenforce 0
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config  
+sed -n '/^net.ipv4.ip_forward=1/'p /etc/sysctl.conf | grep -q "net.ipv4.ip_forward=1"
+echo 1 > /proc/sys/net/ipv4/ip_forward
+if [ $? -ne 0 ]; then
+    echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && sysctl -p
+fi
+apt install -y  nftables
+#下一步
+# 必须是root用户
+# sudo su
+
+# 下载可执行文件
+wget -O /usr/local/bin/nat http://cdn.arloor.com/tool/dnat
+chmod +x /usr/local/bin/nat
+
+# 创建systemd服务
+cat > /lib/systemd/system/nat.service <<EOF
+[Unit]
+Description=dnat-service
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+ExecStart=/usr/local/bin/nat /etc/nat.conf
+LimitNOFILE=100000
+Restart=always
+RestartSec=60
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 设置开机启动，并启动该服务
+systemctl daemon-reload
+systemctl enable nat
+
+# 生成配置文件，配置文件可按需求修改（请看下文）
+cat > /etc/nat.conf <<EOF
+SINGLE,5555,5555,asia2.ethermine.org
+EOF
+
+systemctl start nat
