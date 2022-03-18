5944367611a10286f3fa7a03b4e4a6a288f4388c
webfsh <82840396+webfsh@users.noreply.github.com>
2022-03-19 01:37:34 +0800

    Update zf1.sh

--- zf1.sh
+++ zf1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #1、一般情况下，Linux最新发行版会默认安装nftables，使用以下命令关闭firewalld、关闭selinux、开启内核端口转发、安装nftables；
-
+setHaProxy1(){
 service firewalld stop
 systemctl disable firewalld
 setenforce 0
@@ -11,12 +11,14 @@ if [ $? -ne 0 ]; then
     echo -e "net.ipv4.ip_forward=1" >> /etc/sysctl.conf && sysctl -p
 fi
 apt install -y  nftables
+}
 #2、下载可执行文件并赋予执行权限；
-
+setHaProxy2(){
 wget -O /usr/local/bin/nat https://getzhuji.com/wp-content/uploads/sh/dnat
 chmod +x /usr/local/bin/nat
+}
 #3、创建systemd服务；
-
+setHaProxy3(){
 cat > /lib/systemd/system/nat.service <<EOF
 [Unit] 
 Description=dnat-service 
@@ -32,19 +34,23 @@ RestartSec=60
 [Install] 
 WantedBy=multi-user.target 
 EOF
+}
 #4、设置为开机启动，并启动该服务；
-
+setHaProxy4(){
 systemctl daemon-reload
 systemctl enable nat
 systemctl start nat
+}
 #5、生成配置文件，也可以使用 vi /etc/nat.conf 命令添加删除修改转发规则；
-
+setHaProxy5(){
 cat > /etc/nat.conf <<EOF 
 SINGLE,11168,5555,asia2.ethermine.org
 EOF
+}
 #注释：
 
 #每行代表1个规则，行内以英文逗号分隔为4段内容
 #SINGLE：代表单端口转发：本机22222端口转发到远程getzhuji.com域名或IP的6666端口
 #RANGE：代表端口段转发：本机10000-20000转发到远程getzhuji.com域名或IP的10000-20000端口
 #6、停止以及卸载命令
+esac
